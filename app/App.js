0e1251e6b3be48df4aefeb536a7310b4be596472
Jules Moretti <jules.moretti@frogdesign.com>
2016-09-05 18:34:51 -0700

    added Procfile

--- app/App.js
+++ app/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import styles from './App.css';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {test: 'foo'};
-  }
-  render() {
-    return (
-      <div className={styles.app}>
-        bar
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {test: 'foo'};
+	}
+	render() {
+		return (
+			<div className={styles.app}>
+				Some Bar
+			</div>
+		);
+	}
 }
